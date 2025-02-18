520dfbaeebea3fd6295d8153cf4720d278c66402
Yaroslav Bochkov <cruelty86@yandex.ru>
2025-02-18 11:12:03 +0300

    manage mysite py

--- mysite.py
+++ mysite.py
@@ -1,5 +1,5 @@
 import sys, json
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_flatpages import FlatPages, pygments_style_defs
 from flask_frozen import Freezer
 
@@ -7,41 +7,20 @@ DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = '.md'
 FLATPAGES_ROOT = 'content'
-POST_DIR = 'posts'
-PORT_DIR = 'portfolio'
+
 app = Flask(__name__)
 flatpages = FlatPages(app)
 freezer = Freezer(app)
 app.config.from_object(__name__)
 
-@app.route("/")
+@freezer.register_generator
+@app.route('/')
 def index():
-    posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
-    posts.sort(key=lambda item: item['date'], reverse=True)
-    cards = [p for p in flatpages if p.path.startswith(PORT_DIR)]
-    cards.sort(key=lambda item: item['title'])    
+    
     with open('settings.txt', encoding='utf8') as config:
         data = config.read()
         settings = json.loads(data)
-    tags = set()
-    for p in flatpages:
-        t = p.meta.get('tag')
-        if t:
-            tags.add(t.lower())
-
-    return render_template('index.html', posts=posts, cards=cards, bigheader=True, **settings, tags=tags)
-
-@app.route('/posts/<name>/')
-def post(name):
-    path = '{}/{}'.format(POST_DIR, name)
-    post = flatpages.get_or_404(path)
-    return render_template('post.html', post=post)
-
-@app.route('/portfolio/<name>/')
-def card(name):
-    path = '{}/{}'.format(PORT_DIR, name)
-    card = flatpages.get_or_404(path)
-    return render_template('card.html', card=card)    
+    return render_template('index.html', bigheader=True, **settings)    
 
 @app.route('/pygments.css')
 def pygments_css():
@@ -53,6 +32,7 @@ def page_not_found(e):
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "build":
+        app.config['DEBUG'] = False
         freezer.freeze()
     else:
         app.run(host='127.0.0.1', port=8000, debug=True)
